f835748fe6bbce3dff86212e21bf49ba524018ec
Alexander Kubyshkin <alexander.kubyshkin@gmail.com>
2014-08-03 04:46:15 +0700

    add spring support, add pooling

--- src/main/java/cft2014/runner/BootstrapListener.java
+++ src/main/java/cft2014/runner/BootstrapListener.java
@@ -5,41 +5,49 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
 import javax.sql.DataSource;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import cft2014.runner.entity.Client;
 
-public class BootstrapListener implements ServletContextListener {
+//public class BootstrapListener implements ServletContextListener {
+public class BootstrapListener extends org.springframework.web.context.ContextLoaderListener {
+
+  @Autowired
+  private SessionFactory sessionFactory;
+
   @Override
   public void contextInitialized(ServletContextEvent arg0) {
     System.out.println("BootstrapListener: onContextInitialized");
-    // initDatabase("JDBC");
-    initDatabase("Hibernate");
+    super.contextInitialized(arg0);
+    // initDatabase("JDBC", null);
+    // initDatabase("Hibernate", null);
+    initDatabase("SpringHibernate", arg0);
   }
 
   @Override
   public void contextDestroyed(ServletContextEvent arg0) {
+    super.contextDestroyed(arg0);
     System.out.println("BootstrapListener: onContextDestroyed");
   }
 
-  private void initDatabase(String type){
+  private void initDatabase(String type, ServletContextEvent arg0) {
     if ("JDBC".equals(type)) {
       initJDBCDatabase();
     } else if ("Hibernate".equals(type)) {
       initHibernateDatabase();
+    } else if ("SpringHibernate".equals(type)) {
+      initSpringHibernateDatabase(arg0);
     }
   }
 
   private void initJDBCDatabase() {
     DataSource ds = AppUtil.getDataSource();
-    // JDBCDataSource ds = new JDBCDataSource();
-    // ds.setUrl("jdbc:hsqldb:mem:clients");
-    // ds.setUser("sa");
-    // ds.setPassword("");
 
     try (Connection c = ds.getConnection();) {
       String createTable = "CREATE TABLE CLIENTS (ID INTEGER NOT NULL IDENTITY, "
@@ -61,8 +69,19 @@ public class BootstrapListener implements ServletContextListener {
   private void initHibernateDatabase() {
     SessionFactory sf = AppUtil.getSessionFactory();
     Session session = sf.openSession();
+    initHibernateDatabase(session);
+  }
+
+  private void initHibernateDatabase(Session session) {
     session.save(new Client("Alexander", "Sergeevich", "Kubyshkin"));
     session.save(new Client("Anastasiya", "Sergeevna", "Kubyshkina"));
     session.close();
+  }
+
+  private void initSpringHibernateDatabase(ServletContextEvent arg0) {
+    WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(arg0.getServletContext());
+    SessionFactory sf = ctx.getBean(SessionFactory.class);
+    Session s = sf.openSession();
+    initHibernateDatabase(s);
   }
 }
